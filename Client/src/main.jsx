fe5b05a9289b67b3b8c3bb7ebcf145b9f1be9639
devops-prince <dev.prince116@gmailcom>
2024-08-28 03:41:09 +0530

    client server added

---
+++ Client/src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ToastContainer />
+    <App />
+  </BrowserRouter>
+);
